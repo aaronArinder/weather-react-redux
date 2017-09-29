789df414eeffbaf860394f2b10a661f8656107ba
Aaron Arinder <aaronarinder@gmail.com>
2017-09-29 10:10:25 -0400

    keep it secret, keep it safe! hiding API key for best practices

---
+++ secrets/API_keys.js
@@ -0,0 +1,1 @@
+export default 'f27ded5a9118a5d4f76b55af5e699b56';
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
+import API_KEY from '../../secrets/API_keys';
 
-const API_KEY = 'f27ded5a9118a5d4f76b55af5e699b56';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
